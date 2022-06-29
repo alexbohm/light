a80ef3a9d134c9e28dc854dbbe7f6530c5a30b9c
Alex Bohm <aabohm7@gmail.com>
2022-06-29 01:34:46 +0000

    #4 SK9822 LED

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,39 @@
 
 use panic_halt as _;
 
+mod state_machine;
+
 #[rtic::app(device = stm32f4xx_hal::pac)]
 mod app {
+    use crate::state_machine::LightStateMachine;
     use stm32f4xx_hal::{
+        dma::{
+            config::DmaConfig, traits::StreamISR, MemoryToPeripheral, Stream2, StreamsTuple,
+            Transfer,
+        },
         gpio::{Edge, Input, Output, PushPull, PA0, PA10, PC13},
+        pac::{DMA2, SPI1},
         prelude::*,
+        spi::{Mode, NoMiso, Phase, Polarity, Spi, Tx},
+        timer::Event,
     };
 
+    const ARRAY_SIZE: usize = 44;
+
+    type TxTransfer =
+        Transfer<Stream2<DMA2>, 2, Tx<SPI1>, MemoryToPeripheral, &'static mut [u8; ARRAY_SIZE]>;
     #[shared]
     struct Shared {
+        state_machine: LightStateMachine,
         led: PC13<Output<PushPull>>,
+        tx_transfer: TxTransfer,
     }
 
     #[local]
     struct Local {
         button: PA0<Input>,
         pir_sensor: PA10<Input>,
+        on_deck_tx_buffer: Option<&'static mut [u8; ARRAY_SIZE]>,
     }
 
     #[init]
@@ -29,23 +46,30 @@ mod app {
 
         // Configure the clocks for the device. STM32CubeMX is very helpful for visualizing these values.
         let rcc = ctx.device.RCC.constrain();
-        let _clocks = rcc
+        let clocks = rcc
             .cfgr
             // The Black Pill board has a 25 MHz high speed external (HSE) crystal.
             .use_hse(25.MHz())
-            .require_pll48clk()
-            // Set sysclk to maximum of 100Mhz
-            .sysclk(100.MHz())
-            .hclk(100.MHz())
-            // Set pclk1 to maximum of 50Mhz.
-            .pclk1(50.MHz())
-            // Set pclk2 to maximum of 100Mhz.
-            .pclk2(100.MHz())
+            // Set sysclk frequency.
+            .sysclk(50.MHz())
+            .hclk(50.MHz())
+            // Set pclk1 frequency.
+            .pclk1(25.MHz())
+            // Set pclk2 frequency.
+            .pclk2(50.MHz())
+            // Initialize the hardware.
             .freeze();
 
         // Initialize the GPIO blocks.
         let gpioa = ctx.device.GPIOA.split();
         let gpioc = ctx.device.GPIOC.split();
+
+        // Initialize our timer.
+        let mut timer = ctx.device.TIM2.counter_us(&clocks);
+        // Enable interrupts.
+        timer.listen(Event::Update);
+
+        let state_machine = LightStateMachine::new(timer);
 
         // Initialize the onboard LED.
         let mut led = gpioc.pc13.into_push_pull_output();
@@ -57,49 +81,171 @@ mod app {
         button.enable_interrupt(&mut ctx.device.EXTI);
         button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
 
-        // Initialize the PIR sensor connected to PA10 to interrupt on both rising and falling edges.
+        // Initialize the PIR sensor connected to PA10 to interrupt on both rising edge.
         let mut pir_sensor = gpioa.pa10.into_floating_input();
         pir_sensor.make_interrupt_source(&mut syscfg);
         pir_sensor.enable_interrupt(&mut ctx.device.EXTI);
-        pir_sensor.trigger_on_edge(&mut ctx.device.EXTI, Edge::RisingFalling);
+        pir_sensor.trigger_on_edge(&mut ctx.device.EXTI, Edge::Rising);
+
+        // SPI Initialization.
+        let spi = ctx.device.SPI1;
+
+        let sck = gpioa.pa5.into_alternate();
+        let pico = gpioa.pa7.into_alternate();
+
+        let mode = Mode {
+            polarity: Polarity::IdleLow,
+            phase: Phase::CaptureOnSecondTransition,
+        };
+
+        let spi1 = Spi::new(spi, (sck, NoMiso {}, pico), mode, 30.MHz(), &clocks);
+
+        let tx = spi1.use_dma().tx();
+
+        let streams = StreamsTuple::new(ctx.device.DMA2);
+        let tx_stream = streams.2;
+
+        let tx_buffer = cortex_m::singleton!(: [u8; ARRAY_SIZE] = [0; ARRAY_SIZE]).unwrap();
+        let on_deck_tx_buffer = cortex_m::singleton!(: [u8; ARRAY_SIZE] = [0; ARRAY_SIZE]).unwrap();
+
+        const DRIVE_CURRENT: u8 = 0b00010;
+
+        for index in (4..ARRAY_SIZE).step_by(4) {
+            tx_buffer[index + 0] = 0b11100000 | DRIVE_CURRENT;
+            tx_buffer[index + 1] = state_machine.get_brightness();
+            tx_buffer[index + 2] = state_machine.get_brightness();
+            tx_buffer[index + 3] = state_machine.get_brightness();
+
+            on_deck_tx_buffer[index + 0] = 0b11100000 | DRIVE_CURRENT;
+        }
+        for index in ARRAY_SIZE - 4..ARRAY_SIZE {
+            tx_buffer[index] = 0xff;
+
+            on_deck_tx_buffer[index] = 0xff;
+        }
+
+        let mut tx_transfer = Transfer::init_memory_to_peripheral(
+            tx_stream,
+            tx,
+            tx_buffer,
+            None,
+            DmaConfig::default()
+                .memory_increment(true)
+                .fifo_enable(true)
+                .fifo_error_interrupt(true)
+                .transfer_complete_interrupt(true),
+        );
+
+        tx_transfer.start(|_tx| {});
 
         // Pass the hardware to the RTIC abstraction.
         (
-            Shared { led },
-            Local { button, pir_sensor },
+            Shared {
+                state_machine: state_machine,
+                led: led,
+                tx_transfer: tx_transfer,
+            },
+            Local {
+                button: button,
+                pir_sensor: pir_sensor,
+                // timer: tim,
+                on_deck_tx_buffer: Some(on_deck_tx_buffer),
+            },
             init::Monotonics(),
         )
     }
 
+    /// Put the CPU to sleep to conserve power until the next interrupt.
+    ///
+    /// This uses the Wait For Interrupt (WFI) assembly instruction to put the cpu into a low power state.
+    ///
+    /// Arm Documentation
+    /// https://developer.arm.com/documentation/ddi0439/b/Nested-Vectored-Interrupt-Controller/NVIC-functional-description/Low-power-modes
+    fn cpu_sleep_mode() {
+        cortex_m::asm::wfi();
+    }
+
     #[idle]
     fn idle(_ctx: idle::Context) -> ! {
-        // TODO: Loop for now. Eventually this should put the hardware to sleep to conserve power.
-        #[allow(clippy::empty_loop)]
-        loop {}
+        loop {
+            cpu_sleep_mode();
+        }
     }
     /// Handle interrupts for the onboard button.
-    #[task(binds = EXTI0, local = [button], shared = [led])]
+    #[task(binds = EXTI0, local = [button], shared = [state_machine, led, tx_transfer])]
     fn button_click(mut ctx: button_click::Context) {
         if ctx.local.button.check_interrupt() {
             ctx.local.button.clear_interrupt_pending_bit();
 
-            // Toggle the LED atomically.
-            ctx.shared.led.lock(|led| {
-                led.toggle();
-            })
+            ctx.shared.state_machine.lock(|state_machine| {
+                state_machine.ir_sensor_triggered();
+            });
         }
     }
 
     /// Handle interrupts for the PIR sensor.
-    #[task(binds = EXTI15_10, local = [pir_sensor], shared = [led])]
+    #[task(binds = EXTI15_10, local = [pir_sensor], shared = [state_machine, led])]
     fn sensor_trigger(mut ctx: sensor_trigger::Context) {
         if ctx.local.pir_sensor.check_interrupt() {
             ctx.local.pir_sensor.clear_interrupt_pending_bit();
 
-            // Toggle the LED atomically.
-            ctx.shared.led.lock(|led| {
-                led.toggle();
-            })
+            ctx.shared.state_machine.lock(|state_machine| {
+                state_machine.ir_sensor_triggered();
+            });
         }
+    }
+
+    #[task(binds = DMA2_STREAM2, local = [], shared = [tx_transfer, led])]
+    fn dma2_stream2_isr(mut ctx: dma2_stream2_isr::Context) {
+        if Stream2::<DMA2>::get_fifo_error_flag() {
+            ctx.shared.tx_transfer.lock(|dma| {
+                dma.clear_fifo_error_interrupt();
+            });
+        }
+        if Stream2::<DMA2>::get_transfer_complete_flag() {
+            ctx.shared.tx_transfer.lock(|dma| {
+                dma.clear_transfer_complete_interrupt();
+            });
+        }
+    }
+
+    #[task(binds = TIM2, shared = [state_machine, tx_transfer], local = [on_deck_tx_buffer])]
+    fn tim2_isr(mut ctx: tim2_isr::Context) {
+        ctx.shared.state_machine.lock(|state_machine| {
+            let event = state_machine.fade_timer.get_interrupt();
+            if event | Event::Update == Event::Update {
+                state_machine.fade_timer.clear_interrupt(Event::Update);
+
+                state_machine.timer_expired();
+            }
+            if !event.is_empty() {
+                state_machine
+                    .fade_timer
+                    .clear_interrupt(Event::C1 | Event::C2 | Event::C3 | Event::C4);
+            }
+        });
+
+        // Grab the brightness from the state machine.
+        let brightness = ctx
+            .shared
+            .state_machine
+            .lock(|state_machine| state_machine.get_brightness());
+
+        // Update the buffer with the new brightness.
+        let buffer = ctx.local.on_deck_tx_buffer.as_mut().unwrap().as_mut();
+        for index in (4..ARRAY_SIZE).step_by(4) {
+            buffer[index + 1] = brightness;
+            buffer[index + 2] = brightness;
+            buffer[index + 3] = brightness;
+        }
+
+        // Begin transmission of the updated buffer to the LED strip and swap the old buffer into the on deck spot.
+        let old_buffer = ctx.shared.tx_transfer.lock(|spi_dma| {
+            let (result, _) = spi_dma
+                .next_transfer(ctx.local.on_deck_tx_buffer.take().unwrap())
+                .unwrap();
+            result
+        });
+        *ctx.local.on_deck_tx_buffer = Some(old_buffer);
     }
 }
---
+++ src/state_machine.rs
@@ -0,0 +1,104 @@
+use stm32f4xx_hal::{pac::TIM2, prelude::*, timer::Counter};
+
+#[derive(Debug, Clone, Copy)]
+pub enum LightState {
+    On,
+    FadeOff,
+    Off,
+    FadeOn,
+}
+const FADE_OFF_DELAY: u32 = 10 * 1000;
+const FADE_ON_DELAY: u32 = 1 * 1000;
+const TIMEOUT_DELAY: u32 = 5 * 60 * 1000;
+
+pub struct LightStateMachine {
+    pub fade_timer: Counter<TIM2, 1000000>,
+    state: LightState,
+    brightness: u8,
+}
+
+impl LightStateMachine {
+    pub fn new(fade_timer: Counter<TIM2, 1000000>) -> Self {
+        let mut m = LightStateMachine {
+            fade_timer: fade_timer,
+            state: LightState::On,
+            brightness: u8::MAX,
+        };
+
+        m.transition(LightState::On);
+
+        m
+    }
+    pub fn get_state(&self) -> &LightState {
+        &self.state
+    }
+
+    pub fn get_brightness(&self) -> u8 {
+        self.brightness
+    }
+
+    fn transition(&mut self, new_state: LightState) {
+        match new_state {
+            LightState::FadeOff => {
+                const D: u32 = FADE_OFF_DELAY * 1000 / u8::MAX as u32;
+                self.fade_timer.start(D.micros()).unwrap();
+            }
+            LightState::FadeOn => {
+                const D: u32 = FADE_ON_DELAY * 1000 / u8::MAX as u32;
+                self.fade_timer.start(D.micros()).unwrap();
+            }
+            LightState::On => {
+                self.fade_timer.start(TIMEOUT_DELAY.millis()).unwrap();
+            }
+            LightState::Off => {
+                self.fade_timer.cancel().unwrap();
+            }
+        }
+
+        self.state = new_state;
+    }
+
+    pub fn timer_expired(&mut self) {
+        match &self.state {
+            LightState::FadeOff => {
+                self.brightness = self.brightness.saturating_sub(1);
+
+                if self.brightness == 0 {
+                    self.transition(LightState::Off);
+                }
+            }
+            LightState::FadeOn => {
+                self.brightness = self.brightness.saturating_add(1);
+
+                if self.brightness == 255 {
+                    self.transition(LightState::On);
+                }
+            }
+            LightState::On => {
+                self.transition(LightState::FadeOff);
+            }
+            state => {
+                panic!(
+                    "Fade timer expired when not in a state that should have the timer running. state: {:?}", state
+                );
+            }
+        }
+    }
+
+    pub fn ir_sensor_triggered(&mut self) {
+        match &self.state {
+            LightState::On => {
+                self.transition(LightState::On);
+            }
+            LightState::FadeOn => {
+                // Do Nothing.
+            }
+            LightState::FadeOff => {
+                self.transition(LightState::FadeOn);
+            }
+            LightState::Off => {
+                self.transition(LightState::FadeOn);
+            }
+        }
+    }
+}
